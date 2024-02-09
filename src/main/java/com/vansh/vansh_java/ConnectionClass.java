58204d8725ee8b65c1b9a1cd64c3683f4fbd84f9
Vansh Bansal <vanshbansal030@gmail.com>
2024-02-09 10:47:51 +0530

    complete push of project

---
+++ src/main/java/com/vansh/vansh_java/ConnectionClass.java
@@ -0,0 +1,14 @@
+package com.vansh.vansh_java;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection con=null;
+		Class.forName("com.mysql.cj.jdbc.Driver");//Load and register driver
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_22","root","Mysql@1701");
+		System.out.println("Connection Established");
+		return con;
+	}
+}
---
+++ src/test/java/com/vansh/vansh_java/VanshJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vansh.vansh_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanshJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
